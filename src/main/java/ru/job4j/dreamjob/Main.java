f183ef8b507158db9c019f89f87c113780705b39
ElizavetaKr <elizovetakryukova@mail.ru>
2025-01-09 20:20:12 +0300

    1. Генератор HTML [#504838]

--- src/main/java/ru/job4j/dreamjob/Main.java
+++ src/main/java/ru/job4j/dreamjob/Main.java
@@ -3,6 +3,9 @@ package ru.job4j.dreamjob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Данный класс - точка входа в приложение.
+ * Отвечает за запуск встроенного сервера и инициализацию конфигурации приложения.*/
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -3,11 +3,14 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/** Данный класс - Контроллер для обработки запросов на главную страницу.
+ * Предоставляет пользователю стартовую информацию о приложении*/
+
 @Controller
-@SuppressWarnings("unused")
 public class IndexController {
     @GetMapping("/index")
     public String getIndex() {
         return "index";
     }
 }
+
