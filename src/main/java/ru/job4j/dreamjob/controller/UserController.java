e44ec369df79c4a7daa424fa2e18bda3c484d0f7
ElizavetaKr <elizovetakryukova@mail.ru>
2025-02-01 13:11:47 +0300

    3. Filter [#504865 #530821]

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(Model model, @ModelAttribute User user) {
+    public String register(@ModelAttribute User user, Model model) {
         var savedUser = userService.save(user);
         if (savedUser.isEmpty()) {
             model.addAttribute("message", "Пользователь с такой почтой уже существует");
